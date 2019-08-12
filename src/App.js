44c43723cc380bc5d6f43ba853b86152c236638a
PengYi <pengyi@zhihu.com>
2019-08-12 11:47:34 +0800

    fix: 添加 css module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
 
+import Card from './components/Card'
+
 function App() {
   return (
     <div id="monar">
       <header id="header">
         Monar
       </header>
+      <main id="main">
+        <Card />
+        <Card />
+      </main>
+      <footer id="footer">
+        Monar@2019
+      </footer>
     </div>
   )
 }
